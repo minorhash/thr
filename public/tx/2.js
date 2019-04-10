ee1abeb7ed2a5f7550d28f160d40447ed3619b4a
minorhash <minorhash@gmail.com>
2019-04-10 14:58:16 +0900

    04/10/19

--- public/tx/2.js
+++ public/tx/2.js
@@ -2,6 +2,7 @@ var tx1,tx2
 tx1="2.カリキュラム"
 
 tx2="● e-learningに特化したオンラインスクールです。<br>場所、時間に縛られることなく受講が可能です。<br>\
-● 世界各国で導入され実績を上げている信頼のLMS(learning management system)であるmoodleを導入しています。<br>\
+● 世界各国で導入され実績を上げている信頼のLMS、<br>\
+(learning management system)であるmoodleを導入しています。<br>\
 日本でも上智大学、桜美林大学などで導入されています。"
 
